e838999fc65f1e19ddf6ce9e0c23ac360a88b77b
Petar Slavnic <petarslavnic@gmail.com>
2018-04-27 23:35:52 +0200

    Export channel provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import RealTimeProvider from './components/RealTimeProvider'
 import RealTimeChannel from './components/RealTimeChannel'
 import realTimeEventListener from './hoc/realTimeEventListener'
 import realTimeEventTrigger from './hoc/realTimeEventTrigger'
+import ChannelContext from './context/ChannelContext'
 
 export {
   RealTimeProvider,
   RealTimeChannel,
   realTimeEventListener,
   realTimeEventTrigger,
+  ChannelContext,
 }
