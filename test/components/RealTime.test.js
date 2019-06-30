3d131aecd19d747c8963ca2dec9594402f091b63
Petar Slavnic <petarslavnic@gmail.com>
2019-06-30 15:34:18 +0200

    Rewrite test file

--- test/components/RealTime.test.js
+++ test/components/RealTime.test.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { mount, shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import {
   RealTimeProvider,
   RealTimeChannel,
-  realTimeEventListener,
-  realTimeEventTrigger,
+  useRealTimeEventListener,
+  useRealTimeEventTrigger,
 } from '../../src'
 
 describe(`<RealTimeProvider />`, () => {
   let connector, wrapper, unsubscribe, bind, unbind, trigger
-  let MyComponent, MyWrappedComponent, mockEventCallback, MyWrappedComponentTrigger
+  let MyComponent
 
   beforeEach(() => {
     bind = jest.fn()
@@ -20,10 +20,10 @@ describe(`<RealTimeProvider />`, () => {
       subscribe: () => ({ bind, unbind, trigger }),
       unsubscribe,
     }
-    mockEventCallback = jest.fn()
-    MyComponent = () => (<span>Test</span>)
-    MyWrappedComponent = realTimeEventListener(`CommentCreated`)(MyComponent)
-    MyWrappedComponentTrigger = realTimeEventListener(`isWritingComment`)(MyComponent)
+    MyComponent = ({ callback }) => {
+      useRealTimeEventListener(`CommentCreated`, callback)
+      return (<span>Test</span>)
+    }
   })
 
   afterEach(() => {
@@ -32,17 +32,14 @@ describe(`<RealTimeProvider />`, () => {
     unsubscribe = null
     connector = null
     wrapper = null
-    mockEventCallback = null
     MyComponent = null
-    MyWrappedComponent = null
-    MyWrappedComponentTrigger = null
   })
 
   it(`should render`, () => {
     wrapper = mount(
       <RealTimeProvider connector={connector}>
         <RealTimeChannel name="test">
-          <MyWrappedComponent />
+          <MyComponent />
         </RealTimeChannel>
       </RealTimeProvider>
     )
@@ -54,7 +51,7 @@ describe(`<RealTimeProvider />`, () => {
     wrapper = mount(
       <RealTimeProvider connector={connector}>
         <RealTimeChannel name="test">
-          <MyWrappedComponent />
+          <MyComponent />
         </RealTimeChannel>
       </RealTimeProvider>
     )
@@ -63,210 +60,64 @@ describe(`<RealTimeProvider />`, () => {
     expect(unsubscribe).toHaveBeenCalled()
   })
 
-  it(`should accept event name as function`, () => {
-    const mockEventNameAsFunc = jest.fn()
-
-    MyWrappedComponent = realTimeEventListener(mockEventNameAsFunc)(MyComponent)
-    wrapper = mount(
-      <RealTimeProvider connector={connector}>
-        <RealTimeChannel name="test">
-          <MyWrappedComponent id={89} />
-        </RealTimeChannel>
-      </RealTimeProvider>
-    )
-
-    // Actually, we just want to prove that
-    // passed function has been called.
-    expect(mockEventNameAsFunc).toHaveBeenCalled()
-    expect(mockEventNameAsFunc).toHaveBeenCalledWith({ id: 89 })
-  })
-
-  it(`should handle callback`, () => {
-    let mockEventProps = jest.fn()
-    MyWrappedComponent = realTimeEventListener(`CommentCreated`, mockEventCallback, mockEventProps)(MyComponent)
-
-    let memoizedCallback
-
-    connector = {
-      subscribe: () => {
-        return {
-          bind(eventName, callback) {
-            memoizedCallback = callback
-          },
-          unbind,
-        }
-      },
-      unsubscribe,
-    }
+  it(`should call bind on channel when component did mount`, () => {
+    const myCallback = jest.fn()
 
     wrapper = mount(
       <RealTimeProvider connector={connector}>
         <RealTimeChannel name="test">
-          <MyWrappedComponent id={45} name="Hello World" />
-        </RealTimeChannel>
-      </RealTimeProvider>
-    )
-    expect(mockEventCallback).not.toHaveBeenCalled()
-    expect(mockEventProps).not.toHaveBeenCalled()
-    memoizedCallback({ test: 1234 })
-    expect(mockEventCallback).toHaveBeenCalled()
-    expect(mockEventCallback).toHaveBeenCalledWith({ test: 1234 }, { id: 45, name: `Hello World` })
-    expect(mockEventProps).toHaveBeenCalled()
-    expect(mockEventProps).toHaveBeenCalledWith({ test: 1234 }, { id: 45, name: `Hello World` })
-
-    mockEventCallback.mockClear()
-    mockEventProps.mockClear()
-
-    MyWrappedComponent = realTimeEventListener(`CommentCreated`)(MyComponent)
-    wrapper = mount(
-      <RealTimeProvider connector={connector}>
-        <RealTimeChannel name="test">
-          <MyWrappedComponent id={45} name="Hello World" />
-        </RealTimeChannel>
-      </RealTimeProvider>
-    )
-    expect(mockEventCallback).not.toHaveBeenCalled()
-    expect(mockEventProps).not.toHaveBeenCalled()
-    memoizedCallback({ test: 4321 })
-    expect(mockEventCallback).not.toHaveBeenCalled()
-    expect(mockEventProps).not.toHaveBeenCalled()
-
-    mockEventCallback.mockClear()
-    mockEventProps.mockClear()
-
-    // test pass new props
-    mockEventProps = () => ({ test: 5678 })
-    MyWrappedComponent = realTimeEventListener(`CommentCreated`, null, mockEventProps)(MyComponent)
-    wrapper = mount(
-      <RealTimeProvider connector={connector}>
-        <RealTimeChannel name="test">
-          <MyWrappedComponent id={45} name="Hello World" />
-        </RealTimeChannel>
-      </RealTimeProvider>
-    )
-    memoizedCallback({ test: 4321 })
-    expect(wrapper).toMatchSnapshot()
-
-    // test pass undefined props
-    mockEventProps = () => undefined
-    MyWrappedComponent = realTimeEventListener(`CommentCreated`, null, mockEventProps)(MyComponent)
-    wrapper = mount(
-      <RealTimeProvider connector={connector}>
-        <RealTimeChannel name="test">
-          <MyWrappedComponent id={45} name="Hello World" />
-        </RealTimeChannel>
-      </RealTimeProvider>
-    )
-    memoizedCallback({ test: 4321 })
-    expect(wrapper).toMatchSnapshot()
-    // call it again to test more if-s cases in function
-    memoizedCallback({ test: 4321 })
-    expect(wrapper).toMatchSnapshot()
-
-    // test pass empty props
-    mockEventProps = () => ({})
-    MyWrappedComponent = realTimeEventListener(`CommentCreated`, null, mockEventProps)(MyComponent)
-    wrapper = mount(
-      <RealTimeProvider connector={connector}>
-        <RealTimeChannel name="test">
-          <MyWrappedComponent id={45} name="Hello World" />
-        </RealTimeChannel>
-      </RealTimeProvider>
-    )
-    memoizedCallback({ test: 4321 })
-    expect(wrapper).toMatchSnapshot()
-    // call it again to test more if-s cases in function
-    memoizedCallback({ test: 4321 })
-    expect(wrapper).toMatchSnapshot()
-
-    // test pass same props
-    mockEventProps = () => ({ test: 4321 })
-    MyWrappedComponent = realTimeEventListener(`CommentCreated`, null, mockEventProps)(MyComponent)
-    wrapper = mount(
-      <RealTimeProvider connector={connector}>
-        <RealTimeChannel name="test">
-          <MyWrappedComponent id={45} name="Hello World" />
-        </RealTimeChannel>
-      </RealTimeProvider>
-    )
-    memoizedCallback({ test: 4321 })
-    expect(wrapper).toMatchSnapshot()
-    // call it again to test more if-s cases in function
-    memoizedCallback({ test: 4321 })
-    expect(wrapper).toMatchSnapshot()
-  })
-
-  it(`should handle receive new props`, () => {
-    MyWrappedComponent = realTimeEventListener(`CommentCreated`)(MyComponent)
-
-    let channel = {
-      bind: () => null,
-      unbind: () => null,
-    }
-
-    wrapper = shallow(
-      <MyWrappedComponent id={45} name="Hello World" />,
-      { context: { channel } }
-    )
-
-    wrapper.setProps({ id: 51 })
-
-    expect(wrapper).toMatchSnapshot()
-  })
-
-  it(`should trigger on hijacked function`, () => {
-    let mockOnClick = jest.fn()
-    let mapEventProps = (trigger, { onClick }) => {
-      return {
-        onClick(...attrs) {
-          trigger()
-          onClick(...attrs)
-        },
-      }
-    }
-
-    MyComponent = ({ onClick }) => (<span onClick={onClick}>Test</span>)
-    MyWrappedComponentTrigger = realTimeEventTrigger(mapEventProps)(MyComponent)
-
-    wrapper = mount(
-      <RealTimeProvider connector={connector}>
-        <RealTimeChannel name="test">
-          <MyWrappedComponentTrigger id={45} name="Hello World" onClick={mockOnClick} />
+          <MyComponent callback={myCallback}/>
         </RealTimeChannel>
       </RealTimeProvider>
     )
 
-    expect(mockOnClick).not.toHaveBeenCalled()
-    expect(trigger).not.toHaveBeenCalled()
-    wrapper.find(`span`).first().simulate(`click`)
-    expect(mockOnClick).toHaveBeenCalled()
-    expect(trigger).toHaveBeenCalled()
+    expect(bind).toHaveBeenCalled()
+    expect(bind).toHaveBeenCalledWith(`CommentCreated`, myCallback)
+
     wrapper.unmount()
   })
 
-  it(`should handle real time event trigger receive new props`, () => {
-    let mapEventProps = (trigger, { onClick }) => {
-      return {
-        onClick(...attrs) {
-          trigger()
-          onClick(...attrs)
-        },
-      }
-    }
-    MyWrappedComponent = realTimeEventTrigger(mapEventProps)(MyComponent)
+  it(`should call unbind on channel when component will unmount`, () => {
+    const myCallback = jest.fn()
 
-    let channel = {
-      bind: () => null,
-      unbind: () => null,
-    }
-
-    wrapper = shallow(
-      <MyWrappedComponent id={45} name="Hello World" />,
-      { context: { channel } }
+    wrapper = mount(
+      <RealTimeProvider connector={connector}>
+        <RealTimeChannel name="test">
+          <MyComponent callback={myCallback}/>
+        </RealTimeChannel>
+      </RealTimeProvider>
     )
 
-    wrapper.setProps({ id: 51 })
+    expect(unbind).not.toHaveBeenCalled()
 
-    expect(wrapper).toMatchSnapshot()
+    wrapper.unmount()
+
+    expect(unbind).toHaveBeenCalled()
+    expect(unbind).toHaveBeenCalledWith(`CommentCreated`, myCallback)
+  })
+
+  it(`should trigger fake event on click`, () => {
+    MyComponent = () => {
+      const trigger = useRealTimeEventTrigger()
+      const handleClick = () => {
+        trigger(`TestEvent`, { test: 1234 })
+      }
+      return (
+        <button className="test" type="button" onClick={handleClick} />
+      )
+    }
+
+    wrapper = mount(
+      <RealTimeProvider connector={connector}>
+        <RealTimeChannel name="test">
+          <MyComponent />
+        </RealTimeChannel>
+      </RealTimeProvider>
+    )
+
+    wrapper.find(`.test`).first().simulate(`click`)
+
+    expect(trigger).toHaveBeenCalled()
+    expect(trigger).toHaveBeenCalledWith(`TestEvent`, { test: 1234 })
   })
 })
