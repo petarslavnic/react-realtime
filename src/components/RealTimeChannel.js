0d048765566a3059351e1b975891facd6311369c
Petar Slavnic <petarslavnic@gmail.com>
2019-06-30 15:33:02 +0200

    Update RealTimeChannel component

--- src/components/RealTimeChannel.js
+++ src/components/RealTimeChannel.js
@@ -1,17 +1,52 @@
-import React from 'react'
-import ChannelProvider from './ChannelProvider'
-import ServiceContext from '../context/ServiceContext'
+import React, { useState, useContext, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { ServiceContext, ChannelContext } from '../context'
 
-const RealTimeChannel = (props) => (
-  <ServiceContext.Consumer>
-    {({ subscribe, unsubscribe }) => (
-      <ChannelProvider
-        {...props}
-        subscribe={subscribe}
-        unsubscribe={unsubscribe}
-      />
-    )}
-  </ServiceContext.Consumer>
-)
+const initialState = {
+  bind: () => {},
+  unbind: () => {},
+  trigger: () => {},
+}
+
+const RealTimeChannel = ({ name, children }) => {
+  const { subscribe, unsubscribe } = useContext(ServiceContext)
+  const [ value, setValue ] = useState(initialState)
+
+  useEffect(
+    () => {
+      const channel = subscribe(name)
+
+      setValue({
+        bind(...attrs) {
+          channel.bind(...attrs)
+        },
+        unbind(...attrs) {
+          channel.unbind(...attrs)
+        },
+        trigger(...attrs) {
+          channel.trigger(...attrs)
+        },
+      })
+
+      return () => {
+        unsubscribe(name)
+      }
+    },
+    [name, subscribe, unsubscribe]
+  )
+
+  return (
+    <ChannelContext.Provider value={value}>
+      {React.Children.only(children)}
+    </ChannelContext.Provider>
+  )
+}
+
+RealTimeChannel.propTypes = {
+  name: PropTypes.string.isRequired,
+  children: PropTypes.any,
+}
+
+RealTimeChannel.displayName = `RealTimeChannel`
 
 export default RealTimeChannel
